0c65ff7e13c4e4382b4aebd32e13a79f736fd8d8
17bsmrtic <bensmrtic@gmail.com>
2020-03-10 22:25:31 -0400

    login page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,23 +1,60 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from "./Home";
-import Registration from "./Registration";
 
-export default function App() {
-  return (
-    <Router>
-      {/* <div> */}
-      {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-      <Switch>
-        <Route path="/registration">
-          <Registration />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-      {/* </div> */}
-    </Router>
-  );
+import React, { Component } from "react";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import logo from "../assets/logo.png";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      loginErrors: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    const { email, password } = this.state;
+
+    console.log("test");
+
+  }
+
+  render() {
+    return (
+
+      
+        <div className = "login">
+          <div className="image">
+          <img
+          
+          src={logo}
+          alt="Logo"
+          />
+          </div>
+          <form onSubmit={this.handleSubmit}>
+          
+          <TextField id="standard-basic" label="Email" /> 
+          <TextField type = "password" id="standard-basic" label="Password" />
+          <Button className = "button" variant="contained">Login</Button> 
+          
+          
+
+        
+          </form>
+        </div>
+        
+    );
+  }
 }
